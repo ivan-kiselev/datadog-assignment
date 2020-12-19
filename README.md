7ddfa1e87f744a09277a8968a37c99dddf54752c
Ivan Kiselev <ikiselev@flowtraders.com>
2020-12-19 22:32:00 +0100

    final combinators and tests
    Signed-off-by: Ivan Kiselev <ikiselev@flowtraders.com>

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-[user-identifier](https://tools.ietf.org/html/rfc1413) is not really parsed as it is not usually used
+<h3>Things to improve</h3>
+
+- [user-identifier](https://tools.ietf.org/html/rfc1413) is not really parsed as it is not usually used
+
+- Error handling:
+
+  - Right now it's not that obvious which part of a string is messed up, errors are propogated from nom library
+  - A lot of errors are resulting in wrapped types (Err(Err(Err(...)))). Monads ftw, but amount of time I have for the assignemnt is not playing in my favour in combination to my knowledge about Monads, so I'll leave it for now.
+  - I took a decision to replace unparsable things with "-" if text and "0" if digits, it is going to be considered later in the statistics part of the program
+    - if failed to parse IP address => 0.0.0.0
+    - If failed to parse user_id => "-"
+    - If failed to parse response code => 0
+
+- parse_response_code and parse_response_size functions are almost identical, they just return slightly different type. It probably is possible to refactor it into using generics, but I'd not risk my time for that at this moment
--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ pub struct Request {
 
 // Example of the time log:
 // 127.0.0.1 user-identifier frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326
-// #[derive(Clone, Default, Debug)]
+#[derive(PartialEq, Debug)]
 pub struct LogEntry {
     pub ip_address: IpAddr,
     pub identifier: String,
@@ -21,7 +21,7 @@ pub struct LogEntry {
     pub timestamp: DateTime<FixedOffset>,
     pub request: Request,
     pub response_code: u16,
-    pub size: u128,
+    pub response_size: u128,
 }
 
 impl Display for Request {
@@ -48,7 +48,7 @@ impl Display for LogEntry {
             self.timestamp,
             self.request,
             self.response_code,
-            self.size
+            self.response_size
         )
     }
 }
@@ -148,6 +148,94 @@ pub(self) mod parsers {
             }
         }
         nom::branch::alt((parse_request_helper_dash, parse_request_helper))(i)
+    }
+
+    fn parse_response_code(i: &str) -> nom::IResult<&str, u16> {
+        fn parse_response_code_helper_dash(i: &str) -> nom::IResult<&str, u16> {
+            match nom::character::complete::char('-')(i) {
+                Err(err) => Err(err),
+                Ok((rest, _)) => Ok((rest, 0)),
+            }
+        }
+        fn parse_response_code_helper(i: &str) -> nom::IResult<&str, u16> {
+            match nom::character::complete::digit1(i) {
+                Ok((rest, code)) => match code.parse::<u16>() {
+                    Ok(code) => Ok((rest, code)),
+                    Err(_) => Ok((rest, 0)),
+                },
+                Err(err) => Err(err),
+            }
+        }
+        nom::branch::alt((parse_response_code_helper_dash, parse_response_code_helper))(i)
+    }
+
+    fn parse_response_size(i: &str) -> nom::IResult<&str, u128> {
+        fn parse_response_size_helper_dash(i: &str) -> nom::IResult<&str, u128> {
+            match nom::character::complete::char('-')(i) {
+                Err(err) => Err(err),
+                Ok((rest, _)) => Ok((rest, 0)),
+            }
+        }
+        fn parse_response_size_helper(i: &str) -> nom::IResult<&str, u128> {
+            match nom::character::complete::digit1(i) {
+                Ok((rest, code)) => match code.parse::<u128>() {
+                    Ok(code) => Ok((rest, code)),
+                    Err(_) => Ok((rest, 0)),
+                },
+                Err(err) => Err(err),
+            }
+        }
+        nom::branch::alt((parse_response_size_helper_dash, parse_response_size_helper))(i)
+    }
+    fn parse_log_entry(i: &str) -> Result<LogEntry, String> {
+        match nom::combinator::all_consuming(nom::sequence::tuple((
+            // 127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326
+            parse_ip_address,    // 127.0.0.1
+            is_whitespace,       // " "
+            parse_identifier,    // "-"
+            is_whitespace,       // " "
+            is_not_whitespace,   // "frank"
+            is_whitespace,       // " "
+            parse_timestamp,     // [10/Oct/2000:13:55:36 -0700]
+            is_whitespace,       // " "
+            parse_request,       // "GET /apache_pb.gif HTTP/1.0"
+            is_whitespace,       // " "
+            parse_response_code, // "200"
+            is_whitespace,       // " "
+            parse_response_size, // "2326"
+        )))(i)
+        {
+            Ok((
+                _,
+                (
+                    ip_address,
+                    _,
+                    identifier,
+                    _,
+                    user_id,
+                    _,
+                    timestamp,
+                    _,
+                    request,
+                    _,
+                    response_code,
+                    _,
+                    response_size,
+                ),
+            )) => Ok(LogEntry {
+                ip_address: ip_address,
+                user_id: String::from(user_id),
+                identifier: identifier,
+                timestamp: timestamp,
+                request: request,
+                response_code: response_code,
+                response_size: response_size,
+            }),
+            Err(err) => Err(format!(
+                "Could not parse log entry: {} because of error: {}",
+                i, err
+            )),
+        }
     }
     //TODO: Implement RFC1413 instead of just -
     fn parse_identifier(i: &str) -> nom::IResult<&str, String> {
@@ -254,6 +342,135 @@ pub(self) mod parsers {
                         protocol: String::from("-")
                     }
                 ))
+            );
+        }
+        #[test]
+        fn parse_log_entry_test() {
+            assert_eq!(
+                parse_log_entry("127.0.0.1 - james [09/May/2018:16:00:39 +0000] \"GET /report HTTP/1.0\" 200 123"),
+                Ok(LogEntry{
+                    ip_address: IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+                    identifier: String::from("-"),
+                    user_id: String::from("james"),
+                    timestamp: DateTime::parse_from_str("09/May/2018:16:00:39 +0000", "%d/%b/%Y:%H:%M:%S %z")
+                    .unwrap(),
+                    request: Request {
+                        method: String::from("GET"),
+                        path: String::from("/report"),
+                        protocol: String::from("HTTP/1.0")
+                    },
+                    response_code: 200,
+                    response_size: 123
+
+                })
+            );
+            assert_eq!(
+                parse_log_entry(
+                    "- - james [09/May/2018:16:00:39 +0000] \"GET /report HTTP/1.0\" 200 123"
+                ),
+                Ok(LogEntry {
+                    ip_address: IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
+                    identifier: String::from("-"),
+                    user_id: String::from("james"),
+                    timestamp: DateTime::parse_from_str(
+                        "09/May/2018:16:00:39 +0000",
+                        "%d/%b/%Y:%H:%M:%S %z"
+                    )
+                    .unwrap(),
+                    request: Request {
+                        method: String::from("GET"),
+                        path: String::from("/report"),
+                        protocol: String::from("HTTP/1.0")
+                    },
+                    response_code: 200,
+                    response_size: 123
+                })
+            );
+            assert_eq!(
+                parse_log_entry(
+                    "- - - [09/May/2018:16:00:39 +0000] \"GET /report HTTP/1.0\" 200 123"
+                ),
+                Ok(LogEntry {
+                    ip_address: IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
+                    identifier: String::from("-"),
+                    user_id: String::from("-"),
+                    timestamp: DateTime::parse_from_str(
+                        "09/May/2018:16:00:39 +0000",
+                        "%d/%b/%Y:%H:%M:%S %z"
+                    )
+                    .unwrap(),
+                    request: Request {
+                        method: String::from("GET"),
+                        path: String::from("/report"),
+                        protocol: String::from("HTTP/1.0")
+                    },
+                    response_code: 200,
+                    response_size: 123
+                })
+            );
+            assert_eq!(
+                parse_log_entry("- - - - - 200 123"),
+                Ok(LogEntry {
+                    ip_address: IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
+                    identifier: String::from("-"),
+                    user_id: String::from("-"),
+                    timestamp: DateTime::parse_from_rfc3339("1970-01-01T00:00:00-00:00").unwrap(),
+                    request: Request {
+                        method: String::from("-"),
+                        path: String::from("-"),
+                        protocol: String::from("-")
+                    },
+                    response_code: 200,
+                    response_size: 123
+                })
+            );
+            assert_eq!(
+                parse_log_entry("- - - - - - 123"),
+                Ok(LogEntry {
+                    ip_address: IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
+                    identifier: String::from("-"),
+                    user_id: String::from("-"),
+                    timestamp: DateTime::parse_from_rfc3339("1970-01-01T00:00:00-00:00").unwrap(),
+                    request: Request {
+                        method: String::from("-"),
+                        path: String::from("-"),
+                        protocol: String::from("-")
+                    },
+                    response_code: 0,
+                    response_size: 123
+                })
+            );
+            assert_eq!(
+                parse_log_entry("100.100.100.100 - - - - - 123"),
+                Ok(LogEntry {
+                    ip_address: IpAddr::V4(Ipv4Addr::new(100, 100, 100, 100)),
+                    identifier: String::from("-"),
+                    user_id: String::from("-"),
+                    timestamp: DateTime::parse_from_rfc3339("1970-01-01T00:00:00-00:00").unwrap(),
+                    request: Request {
+                        method: String::from("-"),
+                        path: String::from("-"),
+                        protocol: String::from("-")
+                    },
+                    response_code: 0,
+                    response_size: 123
+                })
+            );
+            assert_eq!(
+                parse_log_entry("- - - - - - -"),
+                Ok(LogEntry {
+                    ip_address: IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
+                    identifier: String::from("-"),
+                    user_id: String::from("-"),
+                    timestamp: DateTime::parse_from_rfc3339("1970-01-01T00:00:00-00:00").unwrap(),
+                    request: Request {
+                        method: String::from("-"),
+                        path: String::from("-"),
+                        protocol: String::from("-")
+                    },
+                    response_code: 0,
+                    response_size: 0
+                })
             );
         }
     }
