5129e527b5a7d6cb621f48324702d9b4e12d477f
Ivan Kiselev <ikiselev@flowtraders.com>
2020-12-19 16:40:59 +0100

    Parse IP address
    Signed-off-by: Ivan Kiselev <ikiselev@flowtraders.com>

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,7 @@
 use chrono::{DateTime, FixedOffset};
 use std::fmt;
 use std::fmt::Display;
-use std::net::{Ipv4Addr, Ipv6Addr};
-
-#[derive(Debug)]
-pub enum IPAddress {
-    V4(Ipv4Addr),
-    V6(Ipv6Addr),
-}
+use std::net::IpAddr;
 
 pub struct Request {
     pub method: u16,
@@ -19,7 +13,7 @@ pub struct Request {
 // 127.0.0.1 user-identifier frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326
 // #[derive(Clone, Default, Debug)]
 pub struct LogEntry {
-    pub ip_address: IPAddress,
+    pub ip_address: IpAddr,
     pub user_agent: String,
     pub user_id: String,
     pub timestamp: DateTime<FixedOffset>,
@@ -57,9 +51,70 @@ impl Display for LogEntry {
     }
 }
 pub(self) mod parsers {
+    use nom::{error::ErrorKind, Err, IResult};
+    use std::net::{AddrParseError, IpAddr};
+
+    fn not_whitespace(i: &str) -> nom::IResult<&str, &str> {
+        nom::bytes::complete::is_not(" \t")(i)
+    }
+
+    fn parse_ip_address(i: &str) -> nom::IResult<&str, IpAddr> {
+        nom::combinator::map_parser(not_whitespace, ip_parser)(i)
+    }
+
+    fn ip_parser(i: &str) -> nom::IResult<&str, IpAddr> {
+        match i.parse::<IpAddr>() {
+            Ok(addr) => IResult::Ok(("", addr)),
+            Err(_) => IResult::Err(Err::Error(nom::error::Error {
+                input: i,
+                code: ErrorKind::Verify,
+            })),
+        }
+    }
 
     #[cfg(test)]
     mod tests {
         use super::*;
+        use std::net::{Ipv4Addr, Ipv6Addr};
+        #[test]
+        fn test_ip_address_parsing() {
+            assert_eq!(
+                parse_ip_address("127.0.0.1"),
+                Ok(("", IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1))))
+            );
+            assert_eq!(
+                parse_ip_address("::1 -"),
+                Ok((" -", IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1))))
+            );
+            assert_eq!(
+                parse_ip_address("127.0.0.1 - mary [09/May/2018:16:00:42 +0000] \"POST /api/user HTTP/1.0\" 503 12"),
+                Ok((" - mary [09/May/2018:16:00:42 +0000] \"POST /api/user HTTP/1.0\" 503 12", IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1))))
+            );
+            assert_eq!(
+                parse_ip_address("-"),
+                Err(Err::Error(nom::error::Error {
+                    input: "-",
+                    code: ErrorKind::Verify,
+                }))
+            );
+        }
+        #[test]
+        fn not_whitespace_test() {
+            assert_eq!(
+                not_whitespace("before_whitespace after_whitespace"),
+                Ok((" after_whitespace", "before_whitespace"))
+            );
+            assert_eq!(
+                not_whitespace("before_tab\tafter_tab"),
+                Ok(("\tafter_tab", "before_tab"))
+            );
+            assert_eq!(
+                not_whitespace(" after_space"),
+                Err(nom::Err::Error(nom::error::Error::new(
+                    " after_space",
+                    nom::error::ErrorKind::IsNot
+                )))
+            );
+        }
     }
 }
