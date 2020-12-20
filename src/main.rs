08283db7d8eaf1f79ad2e2bec13f95db62958c5b
Ivan Kiselev <ikiselev@flowtraders.com>
2020-12-20 20:07:27 +0100

    command-line arguments
    Signed-off-by: Ivan Kiselev <ikiselev@flowtraders.com>

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
 extern crate clf_parser;
 use chrono::{Duration, Utc};
+use clap::Clap;
 use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
 
+/// Utility to parse a CLF log file
+/// reflects some statistics about rate and different endpoints
+#[derive(Clap)]
+#[clap(version = "0.1", author = "Ivan Kiselev. <kiselev_ivan@pm.me>")]
+struct Opts {
+    /// Sets an interval of refreshing the statistics in seconds, max 256
+    #[clap(short, long, default_value = "10")]
+    interval: u8,
+    /// Path to the file to watch
+    #[clap(short, long, default_value = "/tmp/access.log")]
+    filename: String,
+}
+
 fn main() {
-    let refresh_interval: i64 = 10;
+    let opts: Opts = Opts::parse();
+    let refresh_interval = opts.interval;
+    let filename = opts.filename;
     let (tx_stats, rx_stats): (Sender<i64>, Receiver<i64>) = mpsc::channel();
     thread::spawn(move || {
         clf_parser::read_logs(
             tx_stats,
-            Duration::seconds(refresh_interval),
-            "/tmp/access.log",
+            Duration::seconds(refresh_interval as i64),
+            &filename[..],
         );
     });
 
@@ -19,15 +35,14 @@ fn main() {
         let start = Utc::now().timestamp() as u64;
         let message = rx_stats.recv();
         match message {
-            Ok(messages_count) => println!(
-                "Rate: {}/s",
-                messages_count as f64 / refresh_interval as f64
-            ),
+            Ok(messages_count) => {
+                println!("Rate: {}/s", messages_count as f64 / opts.interval as f64)
+            }
             Err(err) => println!("Error: {}", err),
         }
         let end = Utc::now().timestamp() as u64;
         thread::sleep(std::time::Duration::from_secs(
-            refresh_interval as u64 - (end - start),
+            opts.interval as u64 - (end - start),
         ));
     }
 }
