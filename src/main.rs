921c68f69cdb97b8b21a8fb46834467b68718c1b
Ivan Kiselev <ikiselev@flowtraders.com>
2020-12-20 19:19:43 +0100

    make refresh interval a variable temporary
    Signed-off-by: Ivan Kiselev <ikiselev@flowtraders.com>

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,29 @@ use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
 
 fn main() {
+    let refresh_interval: i64 = 10;
     let (tx_stats, rx_stats): (Sender<i64>, Receiver<i64>) = mpsc::channel();
     let _ = thread::spawn(move || {
-        clf_parser::read_logs(tx_stats, Duration::seconds(10), "/tmp/access.log");
+        clf_parser::read_logs(
+            tx_stats,
+            Duration::seconds(refresh_interval),
+            "/tmp/access.log",
+        );
     });
 
     loop {
         let start = Utc::now().timestamp() as u64;
         let message = rx_stats.recv();
         match message {
-            Ok(messages_count) => println!("Rate: {}/s", messages_count as f64 / 10.0),
+            Ok(messages_count) => println!(
+                "Rate: {}/s",
+                messages_count as f64 / refresh_interval as f64
+            ),
             Err(err) => println!("Error: {}", err),
         }
         let end = Utc::now().timestamp() as u64;
-        thread::sleep(std::time::Duration::from_secs(10 - (end - start)));
+        thread::sleep(std::time::Duration::from_secs(
+            refresh_interval as u64 - (end - start),
+        ));
     }
 }
