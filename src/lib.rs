1f773d3f49ec9016a86f4854d6c1a5e9987d5e2d
Ivan Kiselev <ikiselev@flowtraders.com>
2020-12-23 22:06:53 +0100

    move out stats aggregation function and create datatypes for UI message
    Signed-off-by: Ivan Kiselev <ikiselev@flowtraders.com>

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,26 @@ extern crate logwatcher;
 use chrono::{Duration, Utc};
 use logwatcher::{LogWatcher, LogWatcherAction};
 pub use parser_combinators::*;
+use rbl_circular_buffer::*;
+use std::collections::HashMap;
 use std::io::BufRead;
-use std::sync::mpsc::Sender;
+use std::sync::mpsc::RecvTimeoutError;
+use std::sync::mpsc::{Receiver, Sender};
+
+enum RenderMessage {
+    UI(UIUpdate),
+    Exit(bool),
+}
+
+pub struct UIUpdate {
+    alert: Alert,
+    stats: HashMap<String, i64>,
+}
+
+pub struct Alert {
+    active: bool,
+    value: i64,
+}
 
 // Return range of unix timestamps indicatding now() substracted by refresh interval
 // We +1 in the end because Rust ranges are not inclusive on the right side..
@@ -16,6 +34,7 @@ fn acceptible_timestamps(refresh_interval: Duration) -> std::ops::Range<i64> {
     (Utc::now().timestamp() - refresh_interval.num_seconds())..Utc::now().timestamp() + 1
 }
 
+// Read logs and send log entries through Sender (tx_stats)
 pub fn read_logs(tx_stats: Sender<LogEntry>, refresh_interval: Duration, filename: &str) {
     // Panicing in both cases if can't open the file
     let file = std::fs::File::open(filename).unwrap();
@@ -44,4 +63,82 @@ pub fn read_logs(tx_stats: Sender<LogEntry>, refresh_interval: Duration, filenam
         }
         LogWatcherAction::None
     });
+}
+
+// Receive logs and aggregate them to data
+pub fn collect_stats(
+    rx_stats: Receiver<LogEntry>,
+    refresh_interval: u64,
+    alerting_interval: u64,
+    alert_threshold: u64,
+) {
+    // Hashmap with {section => hit_rate}, where section is '/users'
+    let mut stats: HashMap<String, i64> = HashMap::new();
+
+    // A circular buffer for collect alerting values
+    // pushing to CircularBuffer<T> of len that equels its max capacity results in replacing first element
+    // making it simple FIFO perfectly suitable for keeping values exclusively relevant for alerting
+    let max_alerting_buffer_len = if alerting_interval % refresh_interval != 0 {
+        alerting_interval / refresh_interval + 1
+    } else {
+        alerting_interval / refresh_interval
+    } as usize;
+    let mut alerting_buffer: CircularBuffer<i64> = CircularBuffer::new(max_alerting_buffer_len);
+    // Start counter for watching average values over time
+    let mut start = Utc::now().timestamp() as u64;
+    loop {
+        let message = rx_stats.recv_timeout(std::time::Duration::from_secs(refresh_interval));
+        match message {
+            Ok(log_entry) => {
+                let count = stats
+                    .entry(
+                        log_entry
+                            .request
+                            .path
+                            .clone()
+                            .split('/')
+                            .collect::<Vec<&str>>()[1]
+                            .to_owned(),
+                    )
+                    .or_insert(0);
+                *count += 1;
+            }
+            Err(RecvTimeoutError::Disconnected) => {
+                println!("File-reading thread has failed, check file path and permissions")
+            }
+            Err(RecvTimeoutError::Timeout) => {
+                println!(
+                    "Logs stopped coming, timeout of {} seconds occured!",
+                    refresh_interval
+                )
+            }
+        }
+        let end = Utc::now().timestamp() as u64;
+        if (end - start) >= refresh_interval {
+            start = Utc::now().timestamp() as u64;
+            println!();
+            let mut total = 0;
+            for (key, value) in &stats {
+                println!("Endpoint {} was hit {} times!", key, value);
+                total += value;
+            }
+            // Save amount of requests for the last refresh_interval to compare with alerting_treshold later
+            alerting_buffer.push(total);
+            if alerting_buffer.len() == max_alerting_buffer_len {
+                // Calculate average hits over alert_interval
+                let avg_val_in_alert_interval = alerting_buffer
+                    .collect::<Vec<i64>>()
+                    .into_iter()
+                    .sum::<i64>()
+                    / (max_alerting_buffer_len * refresh_interval as usize) as i64;
+                if avg_val_in_alert_interval >= alert_threshold as i64 {
+                    println!(
+                        "The rate has exceeded treshold, the value: {}!",
+                        avg_val_in_alert_interval
+                    );
+                };
+            }
+            stats = HashMap::default();
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 extern crate clf_parser;
-use chrono::{Duration, Utc};
+use chrono::Duration;
 use clap::Clap;
 use clf_parser::*;
-use rbl_circular_buffer::*;
-use std::collections::HashMap;
 use std::sync::mpsc;
-use std::sync::mpsc::RecvTimeoutError;
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
 
@@ -40,76 +37,10 @@ fn main() {
             &filename[..],
         );
     });
-
-    // A circular buffer for collect alerting values
-    // pushing to CircularBuffer<T> of len that equels its max capacity results in replacing first element
-    // making it simple FIFO perfectly suitable for keeping values exclusively relevant for alerting
-    let max_alerting_buffer_len = if opts.alert_interval % refresh_interval != 0 {
-        opts.alert_interval / refresh_interval + 1
-    } else {
-        opts.alert_interval / refresh_interval
-    } as usize;
-    let mut alerting_buffer: CircularBuffer<i64> = CircularBuffer::new(max_alerting_buffer_len);
-
-    // Hashmap with {section => hit_rate}, where section is '/users'
-    let mut stats: HashMap<String, i64> = HashMap::new();
-
-    // Start counter for watching average values over time
-    let mut start = Utc::now().timestamp() as u64;
-
-    loop {
-        let message = rx_stats.recv_timeout(std::time::Duration::from_secs(opts.refresh_interval));
-        match message {
-            Ok(log_entry) => {
-                let count = stats
-                    .entry(
-                        log_entry
-                            .request
-                            .path
-                            .clone()
-                            .split('/')
-                            .collect::<Vec<&str>>()[1]
-                            .to_owned(),
-                    )
-                    .or_insert(0);
-                *count += 1;
-            }
-            Err(RecvTimeoutError::Disconnected) => {
-                println!("File-reading thread has failed, check file path and permissions")
-            }
-            Err(RecvTimeoutError::Timeout) => {
-                println!(
-                    "Logs stopped coming, timeout of {} seconds occured!",
-                    opts.refresh_interval
-                )
-            }
-        }
-        let end = Utc::now().timestamp() as u64;
-        if (end - start) >= opts.refresh_interval {
-            start = Utc::now().timestamp() as u64;
-            println!();
-            let mut total = 0;
-            for (key, value) in &stats {
-                println!("Endpoint {} was hit {} times!", key, value);
-                total += value;
-            }
-            // Save amount of requests for the last refresh_interval to compare with alerting_treshold later
-            alerting_buffer.push(total);
-            if alerting_buffer.len() == max_alerting_buffer_len {
-                // Calculate average hits over alert_interval
-                let avg_val_in_alert_interval = alerting_buffer
-                    .collect::<Vec<i64>>()
-                    .into_iter()
-                    .sum::<i64>()
-                    / (max_alerting_buffer_len * opts.refresh_interval as usize) as i64;
-                if avg_val_in_alert_interval >= opts.alert_treshold as i64 {
-                    println!(
-                        "The rate has exceeded treshold, the value: {}!",
-                        avg_val_in_alert_interval
-                    );
-                };
-            }
-            stats = HashMap::default();
-        }
-    }
+    collect_stats(
+        rx_stats,
+        opts.refresh_interval,
+        opts.alert_interval,
+        opts.alert_treshold,
+    );
 }
